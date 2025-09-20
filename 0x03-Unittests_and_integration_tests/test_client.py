45eed332c1892cda2c605aab9b864f01f43aa1b9
Your Name <odamephilip966@gmail.com>
2025-09-20 14:25:29 +0000

    Implement TestGithubOrgClient.test_has_license to unit-test GithubOrgClient.has_license.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -70,6 +70,15 @@ class TestGithubOrgClient(unittest.TestCase):
         expected_repos = ["repo1", "repo2", "repo3"]
         self.assertEqual(result, expected_repos)
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test GithubOrgClient.has_license works as expected"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
